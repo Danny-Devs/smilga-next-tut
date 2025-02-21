379a678f36cd38af75c4df619aecca06ba47d94f
Daniel Ahn <et3rnal.d@gmail.com>
2025-02-20 22:59:12 -0800

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-7xl">Hello World</h1>
+      <Link href="/about" className="text-xl text-blue-500 inline-block mt-8">
+        about page
+      </Link>
     </div>
   );
-}
+};
 
 export default HomePage;
